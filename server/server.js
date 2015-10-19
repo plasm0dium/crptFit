cb9f99abe36819f2666820900506e1836a4ddb24
Ricky Walker <rickwalk45@gmail.com>
2015-10-19 15:51:51 -0700

    [fix] add proper dependencies for ionic

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var db = require('./mysql/config');
-var http = require('http');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var passport = require('passport');
 
-var morgan = require('morgan')
+var morgan = require('morgan');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/../client/mobile/www'));
 
 app.listen(port, function(){
   console.log('listening on port...', port);
