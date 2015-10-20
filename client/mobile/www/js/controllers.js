990b0c377a6ebc17184f8fe66201a988ea78b3fb
Ricky Walker <rickwalk45@gmail.com>
2015-10-19 18:25:03 -0700

    [pull] Accept pull request prior to committing new changes

--- client/mobile/www/js/controllers.js
+++ client/mobile/www/js/controllers.js
@@ -10,6 +10,6 @@ angular.module('crptFit.controllers', [])
 
 .controller('ProgressCtrl', [function() { }])
 
-.controller('MessagesCtrl', function($scope) { })
+.controller('MessagesCtrl', [function() { }])
 
 .controller('GroupCtrl', [function() { }])
