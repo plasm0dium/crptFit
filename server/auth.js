8bab94f2cebe4d8447c443d00b8a5801708f8e22
Tedwildberry <plasm0dium@Androids-MacBook-Pro.local>
2015-10-30 09:44:24 -0700

    [doc] change facebook callbackUrl

--- server/auth.js
+++ server/auth.js
@@ -1,5 +1,4 @@
 module.exports = Auth = {
-  clientId: "1656535964587501",
-  clientSecret: "d53a148e59de23865ef871bced0e1fe3",
-  accessToken: "cd16047540cf58626adb172d3f90e2be"
+  clientId: "1659843514256746",
+  clientSecret: "39f65d39dcf26cc6509b19337e072bad"
 }
