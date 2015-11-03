7c64763afd05831b24cc4e069aad517ee85ef4da
Tedwildberry <plasm0dium@Androids-MacBook-Pro.local>
2015-11-02 20:16:28 -0800

    [doc] switch DB back to mysql

--- server/server.js
+++ server/server.js
@@ -558,7 +558,7 @@ app.post('/auth/messages/add:id', function (req, res){
   var userId = req.user.attributes.id;
   var chatId = req.params.id;
   var body = req.body.message;
-  console.log(chatId, 'this is chatID', userId, 'this is user id', body, 'this is body')
+  console.log(chatId, 'this is chatID', userId, 'this is user id', body, 'this is body');
   db.model('Message').newMessage({
     user_id: userId,
     chat_id: chatId,
