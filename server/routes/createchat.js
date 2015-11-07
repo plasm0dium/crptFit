394a681a3f69bb79a5697a5606629b0e75de724c
plasm0dium <my email address used on my git repo>
2015-11-07 14:59:54 -0800

    [doc] modified createchat file

--- server/routes/createchat.js
+++ server/routes/createchat.js
@@ -30,6 +30,15 @@ Createchat.post('/chat/add:id', function (req, res){
         created_at: new Date()
       })
       .save();
+    })
+    .then(function(){
+     db.model('Message').newMessage({
+       user_id: userId1,
+       chat_id: chatId,
+       text: "New Chatroom!",
+       created_at: new Date()
+     })
+     .save();
     });
 });
 
